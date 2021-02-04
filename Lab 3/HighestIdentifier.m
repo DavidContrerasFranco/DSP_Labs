2b41310b8aa27d4bcc3b5be3e2fc5db6cd82020d
DavidContrerasFranco <44550339+DavidContrerasFranco@users.noreply.github.com>
2021-02-04 11:29:34 +0100

    Del Lab3

--- Lab 3/HighestIdentifier.m
+++
@@ -1,7 +0,0 @@
-function Harmonic = HighestIdentifierAyalaContreras(y, Fs)
-    Y = fft(y);
-    [~, argmax] = max(abs(Y));
-    L = length(y);
-    f = Fs*(0:(L/2))/L;
-    Harmonic = f(argmax);
-end
